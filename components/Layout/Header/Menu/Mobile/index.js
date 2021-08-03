bb0e2e403efdfc4720da09b294de011baf33bdc0
DidierSoules <36740732+DidierSoules@users.noreply.github.com>
2021-08-03 17:51:06 -0300

    Create a 'click outside' event (BIL-104) (#16)
    * WIP click outside event.
    
    * feature: add mouseclick outside event
    
    Co-authored-by: Juan Manuel Pernumian <juan.pernumian@gmail.com>

--- components/Layout/Header/Menu/Mobile/index.js
+++ components/Layout/Header/Menu/Mobile/index.js
@@ -1,27 +1,41 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Link from "next/link";
 import Icon from "../../../../Icon";
 
 const Mobile = ({ isToggle, handleOnClick }) => {
-  const icon = isToggle ? 'cross' : 'bars';
+  const elemRef = useRef(null);
+  const icon = isToggle ? "cross" : "bars";
+
+  const handleClickOutside = (e) => {
+    if (isToggle && !e.path.includes(elemRef.current)) {
+      handleOnClick();
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+  }, [isToggle]);
 
   return (
-    <span className="menu-mobile-items">
+    <span ref={elemRef} className="menu-mobile-items">
       <button className="toggle-button" onClick={handleOnClick}>
         <Icon colorFill="#666" type={icon} width={24} height={24} />
       </button>
-      <aside
-        className={`menu-mobile-aside ${isToggle ? 'open' : 'closed'}`}
-      >
+      <aside className={`menu-mobile-aside ${isToggle ? "open" : "closed"}`}>
         <Link href="/pricing">
-          <a className="link" onClick={handleOnClick}>Cotizá</a>
+          <a className="link" onClick={handleOnClick}>
+            Cotizá
+          </a>
         </Link>
         <Link href="/versions">
-          <a className="link" onClick={handleOnClick}>Versiones</a>
+          <a className="link" onClick={handleOnClick}>
+            Versiones
+          </a>
         </Link>
       </aside>
     </span>
-  )
-}
+  );
+};
 
 export default Mobile;
--- components/Layout/Header/index.js
+++ components/Layout/Header/index.js
@@ -5,16 +5,19 @@ import { Desktop, Mobile } from "./Menu";
 const Header = () => {
   const [isToggle, setIsToggle] = useState(false);
 
-  const handleOnClick = () => {
-    setIsToggle(!isToggle);
-  }
+  const handleOnClick = () => setIsToggle(!isToggle);
 
   return (
     <header className="header">
       <nav className="menu">
         <span className="menu-logo">
           <Link href="/">
-            <img className="logo" src="/static/logo-bilog.svg" alt="Bilog" onClick={() => setIsToggle(false)} />
+            <img
+              className="logo"
+              src="/static/logo-bilog.svg"
+              alt="Bilog"
+              onClick={() => setIsToggle(false)}
+            />
           </Link>
         </span>
         <Desktop />
