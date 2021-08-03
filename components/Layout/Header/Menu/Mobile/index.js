3adcd12438f7e7e0d6214766b3535f42d23bf22c
Juan Manuel Pernumian <juan.pernumian@gmail.com>
2021-08-03 13:37:10 -0300

    feature: add mouseclick outside event

--- components/Layout/Header/Menu/Mobile/index.js
+++ components/Layout/Header/Menu/Mobile/index.js
@@ -3,33 +3,39 @@ import Link from "next/link";
 import Icon from "../../../../Icon";
 
 const Mobile = ({ isToggle, handleOnClick }) => {
-  const icon = isToggle ? 'cross' : 'bars';
-
   const elemRef = useRef(null);
+  const icon = isToggle ? "cross" : "bars";
 
-  const handleClickOutside = () => handleOnClick();
+  const handleClickOutside = (e) => {
+    if (isToggle && !e.path.includes(elemRef.current)) {
+      handleOnClick();
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+  };
 
   useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
-  }, []);
+    document.addEventListener("mousedown", handleClickOutside);
+  }, [isToggle]);
 
   return (
-    <span className="menu-mobile-items" ref={elemRef}>
+    <span ref={elemRef} className="menu-mobile-items">
       <button className="toggle-button" onClick={handleOnClick}>
         <Icon colorFill="#666" type={icon} width={24} height={24} />
       </button>
-      <aside
-        className={`menu-mobile-aside ${isToggle ? 'open' : 'closed'}`}
-      >
+      <aside className={`menu-mobile-aside ${isToggle ? "open" : "closed"}`}>
         <Link href="/pricing">
-          <a className="link" onClick={handleOnClick}>Cotizá</a>
+          <a className="link" onClick={handleOnClick}>
+            Cotizá
+          </a>
         </Link>
         <Link href="/versions">
-          <a className="link" onClick={handleOnClick}>Versiones</a>
+          <a className="link" onClick={handleOnClick}>
+            Versiones
+          </a>
         </Link>
       </aside>
     </span>
-  )
-}
+  );
+};
 
 export default Mobile;
